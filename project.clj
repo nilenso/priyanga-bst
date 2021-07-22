a13b259a00b4dd77ea22a14e440c1e3e019826a7
PriyangaPKini <priyangapkini99@gmail.com>
2021-07-22 20:57:18 +0530

    Adds a project lein project template

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject bst "0.1.0-SNAPSHOT"
+  :description "Clojure Library for Self-Balancing BST "
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [clj-http "3.12.3"]]
+  :repl-options {:init-ns bst.core})
---
+++ test/bst/core_test.clj
@@ -0,0 +1,7 @@
+(ns bst.core-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [bst.core]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
