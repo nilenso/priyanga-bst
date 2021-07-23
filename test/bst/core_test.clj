cda8bc448053ce7e2cefe6dc73aefc454730b4f5
PriyangaPKini <priyangapkini99@gmail.com>
2021-07-23 21:32:03 +0530

    Adds test for height

--- test/bst/core_test.clj
+++ test/bst/core_test.clj
@@ -1,6 +1,6 @@
 (ns bst.core-test
   (:require [clojure.test :refer [deftest testing is]]
-            [bst.core :refer [create-bst]]))
+            [bst.core :refer [create-bst height]]))
 
 (deftest create-bst-test
   (let [expected-tree {:root 2
@@ -12,4 +12,13 @@
   (testing "create-bst failed"
     (is (= expected-tree (create-bst [2 1 9 5 7]))))))
 
-
+(deftest height-test
+  (let [tree {:root 2
+                       :left {:root 1, :left nil, :right nil}
+                       :right
+                       {:root 7
+                        :left {:root 5, :left nil, :right nil}
+                        :right {:root 9, :left nil, :right nil}}}
+        expected-height 3]
+    (testing "create-bst failed"
+      (is (= expected-height (height tree))))))
